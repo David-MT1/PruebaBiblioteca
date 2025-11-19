6933b8bc7c4596a188cb3a3385469bed563dd371
Darianna16 <dmendozala@unsa.edu.pe>
2025-11-18 22:51:04 -0500

    subiendo modificaciones

--- Estudiante.java
+++ Estudiante.java
@@ -1,11 +1,46 @@
-public  class Estudiante {
-    String id;
-    String name;
-    Set<String> passedCourses = new HashSet<>(); // cursos aprobados
-    List<String> currentEnrollment = new ArrayList<>(); // ids de cursos matriculados
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-    Estudiante(String id, String name){
-        this.id = id; this.name = name;
-    } {
+public  class Estudiante extends Persona {
+    int creditosAprobados;
+    Set<String> passedCourses = new HashSet<>(); 
+    List<String> currentEnrollment = new ArrayList<>(); 
+
+    Estudiante(String nombre, String correo, String codigo, String telefono,int creditosAprobados){
+        super (nombre,correo,codigo,telefono);
+        
+    }
+    public int getCreditosAprobados () {
+        return creditosAprobados;
+    }
+    public void setCreditosAprobados (int creditos) {
+        this.creditosAprobados=creditos;
+    }
+    public String toString(){
+        return super.toString();
+    }
     
+    public void solicitarMatricula(String curso) {
+        if (currentEnrollment.contains(curso)) {
+            System.out.println("Ya estás matriculado en el curso: " + curso);
+        } else {
+            currentEnrollment.add(curso);
+            System.out.println("Matrícula solicitada para el curso: " + curso);
+        }
+    }
+
+    public void solicitarConvalidacion(String cursoAnterior, String cursoEquivalente) {
+        if (passedCourses.contains(cursoAnterior)) {
+            System.out.println("Convalidación solicitada: '" + cursoAnterior +"' por el curso equivalente '" + cursoEquivalente + "'.");
+        } else {
+            System.out.println("No se puede solicitar convalidación. El curso '" +cursoAnterior + "' no aparece como aprobado.");
+        }
+    }
 }
+
+
+    
+    
+    
--- Secretaria.java
+++ Secretaria.java
@@ -1,3 +1,23 @@
-public class Secretaria {
-    
+public class Secretaria  extends Persona {
+    Secretaria (String nombre, String correo, String codigo, String telefono){
+        super (nombre,correo,codigo,telefono);
+    }
+    public String toString(){
+        return super.toString();
+    }
+
+     public boolean validarConvalidacion(Estudiante est, String cursoAnterior, String cursoEquivalente) {
+        if (!est.passedCourses.contains(cursoAnterior)) {
+            System.out.println("El estudiante no ha aprobado el curso: " + cursoAnterior);
+            return false;
+        }
+
+        if (est.passedCourses.contains(cursoEquivalente)) {
+            System.out.println("El curso equivalente '" + cursoEquivalente + "' ya aparece como aprobado.");
+            return false;
+        }
+
+        System.out.println("Convalidación válida para: " + cursoAnterior + " → " + cursoEquivalente);
+        return true;
+    }
 }
